8816fa13560022341f1321ecca7e5b07e5413523
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-24 16:21:04 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,36 @@
-are_isomorphic(graph1, graph2) {
+function degSeq (graph, vert){
+    let degree = 0;
+    for(let i = 0; i < graph.vert; i++)
+    {
+        if (graph.dir[vert][i] == 1)
+            degree++;
+    }
+    if (graph.dir[vert][vert] == 1) 
+        degree++;
+         
+    return degree;
+}
+
+function numedge(graph){
+    let list = [];
+    let edge = 0;
+    for (let i= 0; i < graph.length; i++){
+        list[i] = [];
+        for(let j = 0; j < graph[i].length; j++) {
+            if(adjMatrix[i][j] === 1){
+               edge++;
+            }
+        }
+    }
+    return edge;
+}
+
+function are_isomorphic (graph1, graph2) {
     if (graph1.length !== graph2.length) return false;
-    
-    return undefined;
+    if (degSeq(graph1) === degSeq(graph2) && numedge(graph1) === numedge(graph2) && graph1.length === graph2.length) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
